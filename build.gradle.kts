ed732eef516e46f0a48ec87566cd8ba4d2dc85e6
Alexander Schwarze <shikami97@gmx.de>
2022-06-25 13:08:00 +0200

    Initial Project Setup

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+import org.jetbrains.compose.compose
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.6.10"
+    kotlin("plugin.serialization") version "1.6.10"
+    id("org.jetbrains.compose") version "1.1.0"
+}
+
+group = "alex.exe.simmi"
+version = "1.0.0"
+
+repositories {
+    google()
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+}
+
+dependencies {
+    implementation(compose.desktop.currentOs)
+}
+
+tasks.withType<KotlinCompile> {
+    kotlin.sourceSets.all {
+        languageSettings.apply {
+            optIn("kotlin.RequiresOptIn")
+            optIn("kotlin.time.ExperimentalTime")
+        }
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "Exe_Simmi"
+            packageVersion = version.toString()
+        }
+    }
+}
