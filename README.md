63e5786f11ce86e531bdedbafba55167579d97e0
Alexander Schwarze <shikami97@gmx.de>
2022-06-25 13:09:48 +0200

    Update README, Exclude App from Main, Remove Template App Stuff

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Exe.Simmi
+# Exe_Simmi
 A Bot for Simmi that takes a Twitch Chat message and will post it on Discord
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,20 +10,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
-@Composable
-@Preview
-fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
-
-    MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
-        }
-    }
-}
-
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
         App()
