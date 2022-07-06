e1cdb09adac41d4fc25cad16e77fa3f62846c45c
Alexander Schwarze <shikami97@gmx.de>
2022-07-06 16:59:15 +0200

    Updated ClipPlayer functionality

--- src/main/kotlin/ClipPlayer.kt
+++ src/main/kotlin/ClipPlayer.kt
@@ -6,32 +6,50 @@ import kotlin.io.path.extension
 import kotlin.io.path.isDirectory
 import kotlin.io.path.name
 
-fun setupClipPlayer(){
-    val clipDirectory = Paths.get(ClipPlayerConfig.clipLocation)
+class ClipPlayer {
+    private var clips = mutableListOf<ClipInformation>()
 
-    if(!clipDirectory.isDirectory()){
-        logger.error("Clip directory is not existent. Please make sure to give the right path")
-        return
-    }
+    fun setupClipPlayer() {
+        val clipDirectory = Paths.get(ClipPlayerConfig.clipLocation)
 
-    val clips = Files.walk(clipDirectory)
-        .filter {
-            it.extension in ClipPlayerConfig.allowedVideoFiles
-        }.map {
-            ClipInformation (
-                name = it.name
-            )
+        if (!clipDirectory.isDirectory()) {
+            logger.error("Clip directory is not existent. Please make sure to give the right path")
+            return
         }
 
-    val oldClips: MutableList<ClipInformation>
-    val playlistFile = Paths.get("data/currentClipPlaylist.json").toFile().also { file ->
-        if (!file.exists()) {
-            file.createNewFile()
-            logger.info("Playlist file created")
-        } else {
-            oldClips = Json.decodeFromString(file.readText())
-            logger.info("Existing playlist file found! Read Data. ${oldClips.joinToString(" | ") { "${it.name}: played = ${it.played}" }}")
+        clips = Files.walk(clipDirectory)
+            .filter {
+                it.extension in ClipPlayerConfig.allowedVideoFiles
+            }.map {
+                ClipInformation(
+                    name = it.name
+                )
+            }.toList()
+
+        if (clips.isEmpty()) {
+            logger.error("No clips in folder ${ClipPlayerConfig.clipLocation}")
+            return
         }
+
+        var oldClips = mutableListOf<ClipInformation>()
+        val playlistFile = Paths.get("data/currentClipPlaylist.json").toFile().also { file ->
+            if (!file.exists()) {
+                file.createNewFile()
+                logger.info("Playlist file created")
+            } else {
+                oldClips = Json.decodeFromString(file.readText())
+                logger.info("Existing playlist file found! Read Data. ${oldClips.joinToString(" | ") { "${it.name}: played = ${it.played}" }}")
+            }
+        }
+
+        // Update video files if they got played or not
+        clips.forEach { clip ->
+            if (oldClips.any { clip.name == it.name }) {
+                clip.played = oldClips.first { it.name == clip.name }.played
+            }
+        }
+
+        logger.info("Clip player setup finished")
+        //File("data/currentClipPlaylist.json").writeText(Json.encodeToString())
     }
-    //File("data/currentClipPlaylist.json").writeText(Json.encodeToString())
 }
--- src/main/kotlin/ClipPlayerConfig.kt
+++ src/main/kotlin/ClipPlayerConfig.kt
@@ -6,6 +6,7 @@ object ClipPlayerConfig {
         load(File("data/clipPlayer.properties").inputStream())
     }
 
-    val clipLocation = properties.getProperty("clip_location")
+    val clipLocation: String = properties.getProperty("clip_location")
     val allowedVideoFiles: List<String> = properties.getProperty("allowed_video_files").split(",")
+    val port = properties.getProperty("port").toInt()
 }
