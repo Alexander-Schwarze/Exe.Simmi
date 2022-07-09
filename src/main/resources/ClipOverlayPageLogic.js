fea014debcab6eaae157e73a5bed6a492cf21f73
Alexander Schwarze <shikami97@gmx.de>
2022-07-09 15:53:29 +0200

    Finished comment documentation

--- src/main/resources/ClipOverlayPageLogic.js
+++ src/main/resources/ClipOverlayPageLogic.js
@@ -4,7 +4,7 @@ window.onload = () => {
     const warning = document.querySelector('#warning');
 
     // on certain video files, for whatever reason, Chromium does not call the ended event at the end, only the pause event
-    // this will cause a normal pause event, if ever needed, to not work    
+    // this will cause a normal pause event, if ever needed, to not work
     videoPlayer.onended = videoPlayer.onpause = () => {
         console.log('Video done, requesting next one...');
         webSocket.send('next video lol');
